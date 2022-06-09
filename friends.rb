64705f0a8b743f24cce9255a33d1b8f5ae30792a
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2022-06-09 17:57:39 -0500

    Initial commit

---
+++ friends.rb
@@ -0,0 +1,13 @@
+friends_age = [27, 33, 35, 37, 41, 42, 49, 73]
+
+# p friends_age[0] > 30
+
+# if friends_age[2] > 30
+#   puts friends_age[2]
+# end
+
+friends_age.each do |friend_age|
+  if friend_age > 30
+    puts friend_age
+  end
+end
